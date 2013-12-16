341c617d9ea399f1fbd4db49024f9fb56b0cb5a9
Kmar <kmar.kmar@mail.ru>
2013-12-16 20:47:38 +0300

    Запись структуры формы в файл
    теперь последняя использованная структура запоминается и
    востанавливается при запуске программы... также коммиты теперь на
    великом Русском языке

--- ServerMain.pas
+++ ServerMain.pas
@@ -35,6 +35,9 @@ var
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  i,o,u:integer;
+  filehandle:THandle;
 begin
   //p:=Formstructure;
   FormStructure:=TFormStructure.Create;
@@ -43,10 +46,54 @@ begin
   //load last form from file if it exists
   if fileexists('kmarstructdata.ksd') then
   begin
+    //load lines into the structure
 
-    //load lines into
+    filehandle:=fileopen( 'kmarstructdata.ksd', fmopenread);
 
-  end;
+    fileread(filehandle, o, sizeof(o));  //got field count
+
+    i:=0;
+    while (o>(i+1)) do i:=formstructure.incdatalength;
+
+    for i:=0 to length(formstructure.structure)-1 do
+    begin //for every field
+      fileread(filehandle, o, sizeof(o));     //got length
+      setlength(formstructure.structure[i].Name, o);
+      fileread(filehandle, PChar(formstructure.structure[i].Name)^, o*sizeof(Char)); //got name
+
+      fileread(filehandle, o, sizeof(o));
+      case o of
+        0: FormStructure.Structure[i].DataType:=Default;
+        1: FormStructure.Structure[i].DataType:=MyInteger;
+        2: FormStructure.Structure[i].DataType:=MyFloat;
+        3: FormStructure.Structure[i].DataType:=MyString;
+      end; //end of case
+
+      fileread(filehandle, o, sizeof(o)); //filter count
+
+      u:=0;
+      while ((u+1)<o) do u:=formstructure.structure[i].incfilterlength;
+
+      for u:=0 to length(FormStructure.structure[i].Filters)-1 do
+      begin
+        fileread(filehandle, o, sizeof(o));
+        case o of
+          0: FormStructure.Structure[i].Filters[u].FilterType:=MinLength;
+          1: FormStructure.Structure[i].Filters[u].FilterType:=MaxLength;
+          2: FormStructure.Structure[i].Filters[u].FilterType:=LessThen;
+          3: FormStructure.Structure[i].Filters[u].FilterType:=MoreThen;
+          4: FormStructure.Structure[i].Filters[u].FilterType:=ExcludeChar;
+          5: FormStructure.Structure[i].Filters[u].FilterType:=IncludeChar;
+        end; //end of case
+
+        fileread(filehandle, o, sizeof(o));
+        setlength(FormStructure.structure[i].Filters[u].FilterValue, o);
+        fileread(filehandle, PChar(FormStructure.structure[i].Filters[u].FilterValue)^, o*sizeof(char)); //got filter value!
+
+      end; //end of for(u)
+
+    end; //end of for(i)
+  end; //end of fileexists
 end;
 
 procedure TForm1.Changeform1Click(Sender: TObject);
@@ -57,15 +104,68 @@ begin
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
-var i,o:integer;
-begin //maybe free all sub objects as well?
- { for i:=0 to length(formstructure.structure)-1 do
+var
+  i,o,u,p:integer;
+  filehandle:THandle;
+
+begin
+
+  //save current structure in some file.
+  fileHandle:=filecreate('kmarstructdata.ksd', fmopenwrite);
+
+
+  o:=length(formstructure.structure);
+  filewrite(filehandle, o, sizeof(o));
+  for i:=0 to length(formstructure.structure)-1 do
   begin
-    o:=length(formstructure.structure[i].Filters)-1;
-    while o<>-1 do o:=formstructure.structure[i].decfilterlength;
-  end;
-  i:= length(formstructure.structure)-1;
-  while i<>-1 do i:=formstructure.decdatalength;}
+    //    formstructure.structure[i].Name, datatype, filter;
+    o:=length(formstructure.structure[i].Name); //name
+    filewrite(filehandle, o,sizeof(o));
+    filewrite(filehandle, PChar(formstructure.structure[i].name)^, o*sizeof(char));
+
+    case formstructure.structure[i].DataType of
+      default:
+        begin
+          o:=0;
+        end;
+      MyInteger:
+        begin
+          o:=1;
+        end;
+      MyFloat:
+        begin
+          o:=2;
+        end;
+      Mystring:
+        begin
+          o:=3;
+        end;
+    end; //end of case
+    filewrite(handle, o, sizeof(o)); //datatype
+
+    o:=length(formstructure.structure[i].Filters);
+    filewrite(filehandle, o, sizeof(o));   //how many filters
+
+    for u:=0 to length(formstructure.structure[i].Filters)-1 do
+    begin
+      //formstructure.structure[i].Filters[u].FilterType, value
+      case formstructure.structure[i].Filters[u].FilterType of
+        MinLength: o:=0;
+        MaxLength: o:=1;
+        LessThen: o:=2;
+        MoreThen: o:=3;
+        ExcludeChar: o:=4;
+        IncludeChar: o:=5;
+      end; //end of case
+      filewrite(filehandle, o, sizeof(o)); //filtertype
+
+      o:=length(formstructure.structure[i].Filters[u].FilterValue);
+      filewrite(filehandle, o, sizeof(o));
+      filewrite(filehandle, PChar(formstructure.structure[i].Filters[u].FilterValue)^, o*sizeof(char)); //filtervalue
+    end; //end of for(u);
+
+  end; //end of for(i)
+  fileclose(filehandle);
 
   FormStructure.Free;
 end;
